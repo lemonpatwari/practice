cf2c1c4c3a6b9a0d342e41224594557fdf6262ba
lemonpatwari <lemonpatwari@gmail.com>
2021-03-23 18:57:36 +0600

    queue add

---
+++ app/Jobs/SendUpComingStudentEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\SendEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendUpComingStudentEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $provider;
+    protected $email;
+    protected $content;
+    protected $campaign_id;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($provider, $email, $content, $campaign_id)
+    {
+        $this->provider = $provider;
+        $this->email = $email;
+        $this->content = $content;
+        $this->campaign_id = $campaign_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->email)->queue(
+            new SendEmail($this->content, 'subject')
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,17 +21,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/time', function () {
+
+
+    $configuration = \App\Models\Provider::whereFromAddress('lemonpatwari@gmail.com')->first();
+
+    if (!is_null($configuration)) {
+        $config = array(
+//            'MAIL_DRIVER' => $configuration->driver,
+//            'MAIL_HOST' => $configuration->host,
+//            'MAIL_PORT' => $configuration->port,
+//            'MAIL_FROM_ADDRESS' => $configuration->from_address,
+//            'MAIL_FROM_NAME' => $configuration->from_name,
+//            'MAIL_ENCRYPTION' => $configuration->enccryption,
+            'username' => $configuration->from_name,
+//            'MAIL_PASSWORD' => $configuration->password,
+        );
+        Config::set('mail', $config);
+    }
+
+
+
+    return date("Y-m-d h:i:s A", '1616511060');
+
+
+});
+
+Route::get('/email', function () {
+    return view('email.email');
+});
+
+
+Route::post('/email-store', [DashboardController::class, 'emailStore'])->name('emailStore');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
-Route::get('/url', [DashboardController::class,'url']);
+Route::get('/url', [DashboardController::class, 'url']);
 
-Route::get('/dashboard', [DashboardController::class,'index']);
-Route::get('/test', [DashboardController::class,'test']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/test', [DashboardController::class, 'test']);
 Route::resource('users', UserController::class);
 Route::resource('roles', RoleController::class);
 Route::resource('permissions', \App\Http\Controllers\PermissionController::class);
