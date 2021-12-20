7038e43c51d4b86c554b43f87f3b5df9277b77e7
Lemon Patwari <lemonpatwari@gmail.com>
2021-12-20 20:54:26 +0600

    controller add

---
+++ app/Http/Controllers/LemonController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\lemon;
+use Illuminate\Http\Request;
+
+class LemonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\lemon  $lemon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(lemon $lemon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\lemon  $lemon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(lemon $lemon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\lemon  $lemon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, lemon $lemon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\lemon  $lemon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(lemon $lemon)
+    {
+        //
+    }
+}
